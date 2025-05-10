41137345b0e0f71c4d538f395b9f9780e0431323
Germán Martínez <germaga92@gmail.com>
2025-05-10 14:12:37 +0100

    Add Dashboard service

---
+++ src/services/dashboard/DashboardService.ts
@@ -0,0 +1,49 @@
+import ApplicationDAO from '../../dao/ApplicationDAO';
+import LoginDAO from '../../dao/LoginDAO';
+import { Application, NewApplication, ProfileLogin } from '../../types';
+
+class DashboardService {
+  constructor(
+    private loginDAO: LoginDAO,
+    private applicationDAO: ApplicationDAO
+  ) {}
+
+  async createApp(application: NewApplication): Promise<Application> {
+    const newApplication = await this.applicationDAO.create(application);
+
+    return newApplication;
+  }
+
+  async getProfileLoginsFromApp({
+    applicationId,
+    startDate,
+    endDate,
+    chainId
+  }: {
+    applicationId: string,
+    startDate: Date,
+    endDate: Date,
+    chainId: number
+  }): Promise<ProfileLogin[] | undefined> {
+    const profileLogins = await this.loginDAO.findProfileLogins({
+      applicationId,
+      startDate,
+      endDate,
+      chainId
+    });
+
+    return profileLogins;
+  }
+
+  async getAppsFromUser({
+    userId
+  }: {
+    userId: string
+  }): Promise<Application[]> {
+    const applications = await this.applicationDAO.findByUserId(userId);
+
+    return applications;
+  }
+}
+
+export default DashboardService;
